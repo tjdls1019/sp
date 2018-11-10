15f2e830cd78851c009aae472312a0458a2529d0
KOITT <KOITT@KOITT-PC>
2018-11-10 15:21:06 +0900

    test주석

--- src/test/java/com/test/sp/DBTest.java
+++ src/test/java/com/test/sp/DBTest.java
@@ -17,17 +17,17 @@ import com.sp.sp.service.AwsTestService;
 
 import lombok.extern.slf4j.Slf4j;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
-@Slf4j
-public class DBTest {
-
-	@Autowired
-	private AwsTestService ats;
-	
-	@Test
-	public void test() throws SQLException {
-		assertEquals(1, ats.selectAwsTestList().size());
-	}
-
-}
+//@RunWith(SpringJUnit4ClassRunner.class)
+//@ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
+//@Slf4j
+//public class DBTest {
+////
+////	@Autowired
+////	private AwsTestService ats;
+////	
+////	@Test
+////	public void test() throws SQLException {
+////		assertEquals(1, ats.selectAwsTestList().size());
+////	}
+//
+//}
